32421f30d108eb67a8ff9c512247f0f203867500
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-08-21 15:08:04 +0530

    feat : fetching banana api
    have fetched translation api which translates text to minion language

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ var btn = document.querySelector("#btn-translate");
 
 var outputDiv = document.querySelector("#div-output");
 
-btn.addEventListener("click",function(){
-    outputDiv.innerText=textArea.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslation(text) {
+    return serverURL + "?" + "text=" + text;
 }
-)
+
+function errorHandler(error) {
+    console.log("error occurred",error);
+    alert("something wrong with the server");
+}
+
+function clickHandler() {
+    var text = textArea.value;
+    fetch(getTranslation(text)).then(response => response.json()).then(json =>
+        outputDiv.innerText = json.contents.translated).catch(errorHandler);
+}
+
+btn.addEventListener("click", clickHandler);
